071b363b0f4161a35f5133641b0020585d0fc8af
Daniel <daniel_academico1998@hotmail.com>
2025-01-15 19:14:55 -0300

    Implement task list UI with dynamic task display and progress tracking

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,39 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Lista de Tarefas'),
         ),
-        body: Container(
+        body: ListView(
+          children: [
+            Task('Math'),
+            Task("Dart"),
+            Task("FLutter"),
+            Task("Python"),
+            Task("Docker"),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
+class Task extends StatefulWidget {
+  final String text;
+  const Task(this.text, {super.key});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  dynamic lv = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
           color: Colors.black,
           child: Stack(
             children: [
@@ -27,33 +59,71 @@ class MyApp extends StatelessWidget {
                 color: Colors.blue,
                 height: 140,
               ),
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      height: 100,
-                      width: 72,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Colors.white,
+                    height: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          color: Colors.black26,
+                          height: 100,
+                          width: 72,
+                        ),
+                        Container(
+                          width: 200,
+                          alignment: Alignment.center,
+                          child: Text(
+                            widget.text,
+                            style: TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              lv++;
+                            });
+                          },
+                          child: const Icon(Icons.arrow_drop_up_outlined),
+                        ),
+                      ],
                     ),
-                    Text("Aprendendo Flutter"),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Icon(Icons.add),
-                    ),
-                  ],
                   ),
-              ),
+                  Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: 200,
+                              child: LinearProgressIndicator(
+                                color: Colors.white70,
+                                value: lv/20,
+                                backgroundColor: Colors.black54,
+                              ),
+                            ),
+                            Text(
+                              "Nível: $lv",
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.white70,
+                              ),
+                            ),
+                          ],
+                        )
+                      ),
+                ],
+              )
             ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-          ),
-      ),
-    );
+        ));
   }
 }
