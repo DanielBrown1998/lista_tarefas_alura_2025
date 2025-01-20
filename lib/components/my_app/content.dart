eb7792de4ea68e387d0069341e92c7b99da643cd
Daniel <daniel_academico1998@hotmail.com>
2025-01-20 20:50:47 -0300

    Refactor Content widget to use default colors and update progress indicator styling

--- lib/components/my_app/content.dart
+++ lib/components/my_app/content.dart
@@ -5,9 +5,7 @@ class Content extends StatefulWidget {
   final String text;
   final String namePicture;
   final int dificulty;
-  Color color_background = Colors.white70;
-  Color color_line = Colors.black54;
-  Content(this.text, this.dificulty,
+  const Content(this.text, this.dificulty,
       {required this.namePicture, super.key});
 
   @override
@@ -16,6 +14,9 @@ class Content extends StatefulWidget {
 
 class _ContentState extends State<Content> {
   int lv = 0;
+  Color colorBackgroundDefault = Colors.blueGrey;
+  Color colorLine = Colors.white;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -62,7 +63,25 @@ class _ContentState extends State<Content> {
                 child: ElevatedButton(
                   onPressed: () {
                     setState(() {
+                      double lineValue = lv / (5 * (widget.dificulty + 1));
                       lv++;
+                      if (lineValue < 0.15) {
+                        colorBackgroundDefault = Colors.red;
+                      } else if (0.15 >= lineValue || lineValue < 0.25) {
+                        colorBackgroundDefault = Colors.orange;
+                      } else if (0.25 >= lineValue || lineValue < 0.45) {
+                        colorBackgroundDefault = Colors.yellow;
+                        colorLine = Colors.black;
+                      } else if (0.45 >= lineValue || lineValue < 0.65) {
+                        colorBackgroundDefault = Colors.lightGreen;
+                        colorLine = Colors.black;
+                      } else if (0.65 >= lineValue || lineValue < 0.85) {
+                        colorBackgroundDefault = Colors.green;
+                        colorLine = Colors.white;
+                      } else {
+                        colorBackgroundDefault = Colors.blue;
+                        colorLine = Colors.white;
+                      }
                     });
                   },
                   child: Column(
@@ -77,31 +96,40 @@ class _ContentState extends State<Content> {
             ],
           ),
         ),
-        Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  width: 200,
-                  child: LinearProgressIndicator(
-                    color: widget.color_line,
-                    value: lv / (5 * (widget.dificulty + 1)),
-                    backgroundColor: widget.color_background,
+        Container(
+          decoration: BoxDecoration(
+            color: colorBackgroundDefault,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(4),
+              bottomRight: Radius.circular(4),
+            ),
+          ),
+          child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    width: 200,
+                    child: LinearProgressIndicator(
+                      color: colorLine,
+                      value: lv / (5 * (widget.dificulty + 1)),
+                      backgroundColor: Colors.white70,
+                    ),
                   ),
-                ),
-                Text(
-                  "Nível: $lv",
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: widget.color_background,
+                  Text(
+                    "Nível: $lv",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: colorLine,
+                    ),
                   ),
-                ),
-              ],
-            )),
+                ],
+              )),
+        ),
       ],
     );
   }
